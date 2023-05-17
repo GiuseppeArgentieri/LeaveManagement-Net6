f2e0d79c4a67ca42ac6b0ca0a8b8d793ab18577e
giuseppeArge <giuseppe.argentieri@atos.net>
2023-05-17 17:59:09 +0200

    Added DB tables and migrations

---
+++ LeaveManagement.web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.web.Data
+{
+    public abstract class BaseEntity //non puoi istanziarla, funziona solo con l'ineritance
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateDatemodified { get; set; }
+    }
+}
