8dbcd23146a30177b8af654f820b97874d40c2b5
GiuseppeArgentieri <giuseppe.arge24@outlook.it>
2023-07-05 00:45:57 +0200

    Added LeaveRequest

---
+++ LeaveManagement.web/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,8 @@
+﻿using LeaveManagement.web.Data;
+
+namespace LeaveManagement.web.Contracts
+{
+    public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+    {
+    }
+}
---
+++ LeaveManagement.web/Repositories/LeaveRequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagement.web.Contracts;
+using LeaveManagement.web.Data;
+
+namespace LeaveManagement.web.Repositories
+{
+    public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
+    {
+        public LeaveRequestRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
